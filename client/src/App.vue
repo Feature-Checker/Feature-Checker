43bdc070a3a7174fa93cb546ea5c70dfa508dca5
mras2303 <marek.kowalonek@gmail.com>
2019-11-07 19:19:49 +0000

    change

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Homeland</router-link> |
       <router-link to="/about">About</router-link>
     </div>
     <router-view/>
