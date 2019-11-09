a90fc58ca5b1f5c417fa16a5205ee6139990b8fb
Damian Orzeł <dam.orzel@gmail.com>
2019-11-09 02:16:10 +0100

    refactoring js files

--- client/src/main.js
+++ client/src/main.js
@@ -3,7 +3,7 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import i18n from './i18n'
+import i18n from "./i18n";
 
 Vue.config.productionTip = false;
 
