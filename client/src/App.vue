f4bbabae6a14a49cc0d90a5a3200f777c028b2a8
Damian Orzeł <dam.orzel@gmail.com>
2019-11-09 01:11:33 +0100

    some style changes

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,32 +1,22 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <div
+      class="headershape"
+    >Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, eum. Rerum praesentium nihil et. Saepe modi facilis nam commodi velit!</div>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+@import "./src/assets/styles/global/global.scss";
+div#app {
+  .headershape {
+    background-image: url(./assets/images/Shapes/HeaderShape.png);
+    background-repeat: no-repeat;
+    background-position: top right;
+    background-size: contain;
+    width: 100%;
+    height: 100vh;
+    font-size: $text-size;
   }
 }
 </style>
