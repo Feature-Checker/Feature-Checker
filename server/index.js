48d476e379452c32be42a7c6212d1f00da90b160
mras2303 <marek.kowalonek@gmail.com>
2019-11-22 23:42:23 +0100

    Add repository entity

--- server/index.js
+++ server/index.js
@@ -2,8 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
 const mongoose = require('mongoose')
-const axios = require('axios')
-const testRoute = require('./routes/test')
+const repositoryRoute = require('./routes/repository')
 
 const app = express()
 app.use(cors())
@@ -13,20 +12,7 @@ mongoose.connect('mongodb://admin:admin123@ds245018.mlab.com:45018/featurechecke
     .then(() => console.log('DB connected'))
     .catch(e => console.log(e))
 
-app.get('/', (req, res) => {
-    res.send('test')
-})
-
-app.use('/tests', testRoute)
-
-app.get('/random', async (req, res) => {
-    try{
-        const { data } = await axios.get('https://jsonplaceholder.typicode.com/todos/1')
-        res.send(data.title);
-    } catch(e) {
-        console.error(e)
-    }
-})
+app.use('/repositories', repositoryRoute)
 
 app.listen(5000, err => {
     console.log('listening on port 5000')
