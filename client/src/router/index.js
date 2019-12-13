d28a33e0fd5a133a9ab0f55badcccb8f598930fd
Damian Orzeł <damian.orzel1995@gmail.com>
2019-12-13 09:12:18 +0100

    added simple 404 page with translation

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from "vue";
 import VueRouter from "vue-router";
 import Home from "../components/Landing/Landing.vue";
 import AddTool from "../views/FormToolAdd/FormToolAdd.vue";
+import Error404 from "../views/Error404/Error.vue";
 // import i18n from "../i18n";
 Vue.use(VueRouter);
 
@@ -27,6 +28,11 @@ const routes = [
         path: "/add_tool",
         name: "addTool",
         component: AddTool
+      },
+      {
+        path: "/*",
+        name: "404",
+        component: Error404
       }
     ]
   }
---
+++ client/src/views/Error404/Error.vue
@@ -0,0 +1,45 @@
+<template>
+  <section class="error-page">
+    <h2>{ 404 }</h2>
+    <h4>{{$t("error-page.errorSubTitle")}}</h4>
+    <p>{{$t("error-page.errorAction")}}</p>
+    <router-link to="/">
+      <button class="btn purple">{{$t("error-page.button")}}</button>
+    </router-link>
+  </section>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.error-page {
+  width: 100vw;
+  height: 100vh;
+  background: url("~@/assets/images/background/404.jpg");
+  background-size: cover;
+  background-position: center;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  h2,
+  h4,
+  p {
+    font-family: $mont;
+  }
+  h2 {
+    font-size: 12em;
+    color: $purpleHeart;
+    margin: 20px 0;
+  }
+  h4 {
+    margin: 0 0 50px 0;
+  }
+  p {
+    margin: 10px 0;
+    color: $white;
+  }
+}
+</style>
