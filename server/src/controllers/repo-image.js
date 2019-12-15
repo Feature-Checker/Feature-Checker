6a7e0dfe07486030177618067ebe2eddba09493a
mras2303 <marek.kowalonek@gmail.com>
2019-12-15 18:57:34 +0100

    little refactor

--- server/src/controllers/repo-image.js
+++ server/src/controllers/repo-image.js
@@ -1,21 +1,16 @@
-const API_KEY = process.env.API_KEY_GOOGLE;
-const CSE_ID = process.env.CSE_ID_GOOGLE;
 const imageSearch = require("image-search-google");
-const client = new imageSearch(CSE_ID, API_KEY);
-const options = {
-    page: 1
-};
 
 const fetchImage = async(req, res) => {
     try {
         const searchPhrase = `${req.query.search} logo`
-
+        const { API_KEY, CSE_ID } = process.env.API_KEY_GOOGLE;
+        const client = new imageSearch(CSE_ID, API_KEY);
+        const options = { page: 1 };
         const searchResult = await client.search(searchPhrase, options)
         res.json(searchResult)
     } catch (err) {
         res.send(err)
     }
-
 }
 
 module.exports = {
