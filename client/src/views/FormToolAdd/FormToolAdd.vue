2ad315b47730f991cc2cd0cb3eba01f84f1cb4ac
Damian Orzeł <damian.orzel1995@gmail.com>
2019-12-16 10:11:32 +0100

    simple loading and search validation in repo add site

--- client/src/views/FormToolAdd/FormToolAdd.vue
+++ client/src/views/FormToolAdd/FormToolAdd.vue
@@ -3,7 +3,13 @@
     <div class="form-box">
       <form action>
         <label for>{{$t("addTool-page.repoSearchLabel")}}</label>
-        <input type="text" />
+        <input type="text" id="startAnimation" v-on:keyup.once="typingAnimation" />
+        <div class="lds-ring" id="lds-ring">
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
         <div class="logo-container">
           <p>{{$t("addTool-page.logoTitle")}}</p>
           <div class="logos">
@@ -52,6 +58,26 @@ export default {
     handleSelectItem(logo) {
       this.selectedLogo = logo.logoImage;
       this.selected = logo.id;
+    },
+    typingAnimation() {
+      const input = document.getElementById("startAnimation");
+      const animationDiv = document.querySelector("#lds-ring");
+      input.addEventListener("keyup", function() {
+        if (input.value !== "") {
+          animationDiv.classList.add("activeAnimation");
+          input.classList.add("searchError");
+        } else {
+          animationDiv.classList.remove("activeAnimation");
+        }
+        if (input.value === "facebook/react") {
+          input.classList.add("searchSuccess");
+          input.classList.remove("searchError");
+          animationDiv.classList.remove("activeAnimation");
+        } else if (input.value === "") {
+          input.classList.remove("searchSuccess");
+          input.classList.remove("searchError");
+        }
+      });
     }
   },
   created() {
@@ -85,6 +111,7 @@ section.add-tool {
     form {
       display: flex;
       flex-direction: column;
+      position: relative;
       label {
         font-size: 1.6em;
         font-family: $mont;
@@ -93,13 +120,19 @@ section.add-tool {
       input {
         background: transparent;
         border: none;
-        border-bottom: 1px solid $white;
+        border-bottom: 2px solid $white;
         font-size: 2em;
         color: $white;
         font-family: $mont;
         margin: 10px 0;
         outline: none;
         padding: 10px 0;
+        &.searchSuccess {
+          border-bottom: 2px solid green;
+        }
+        &.searchError {
+          border-bottom: 2px solid red;
+        }
       }
       button {
         margin: 20px auto;
@@ -146,6 +179,49 @@ section.add-tool {
         }
       }
     }
+  }
+}
+
+.lds-ring {
+  display: inline-block;
+  position: relative;
+  width: 20px;
+  height: 20px;
+  position: absolute;
+  top: 35px;
+  right: 15px;
+  display: none;
+  &.activeAnimation {
+    display: block;
+  }
+}
+.lds-ring div {
+  box-sizing: border-box;
+  display: block;
+  position: absolute;
+  width: 25px;
+  height: 25px;
+  margin: 8px;
+  border: 2px solid #fff;
+  border-radius: 50%;
+  animation: lds-ring 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+  border-color: #fff transparent transparent transparent;
+}
+.lds-ring div:nth-child(1) {
+  animation-delay: -0.45s;
+}
+.lds-ring div:nth-child(2) {
+  animation-delay: -0.3s;
+}
+.lds-ring div:nth-child(3) {
+  animation-delay: -0.15s;
+}
+@keyframes lds-ring {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
   }
 }
 </style>
