bcbde31dfe4ff5e769aa69e191569d2f7cc9d63d
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-12-17 16:00:53 +0100

    Added repo icon handler

--- server/src/routes/repository.js
+++ server/src/routes/repository.js
@@ -1,9 +1,26 @@
 const express = require('express')
 const router = express.Router()
-const Controller = require('../controllers/repository')
+const Controller = require('../controllers/repository');
+
+const multer = require("multer");
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, './icons');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+})
+
+const upload = multer({
+    storage
+});
+
+
 
 router.route('/')
-    .post(Controller.createNew)
+    .post(upload.single("repository"), Controller.createNew)
     .get(Controller.getAll)
 
 router.route('/:repository_id')
