4992f240f2f78ffd321504ced84efa8dbbd360d0
mras2303 <marek.kowalonek@gmail.com>
2019-12-24 08:39:25 +0100

    Add id to image results

--- server/src/controllers/repo-image.js
+++ server/src/controllers/repo-image.js
@@ -1,6 +1,6 @@
 const imageSearch = require("image-search-google");
 
-const fetchImage = async(req, res) => {
+const fetchImage = async (req, res) => {
     try {
         console.log(req.query)
         console.log(req.params)
@@ -10,6 +10,9 @@ const fetchImage = async(req, res) => {
         const client = new imageSearch(CSE_ID_GOOGLE, API_KEY_GOOGLE);
         const options = { page: 1 };
         const searchResult = await client.search(searchPhrase, options)
+        searchResult.map((result, index) => {
+            result.id = index + 1
+        })
         res.json(searchResult)
     } catch (err) {
         res.send(err)
