79e36d315ea820e76ebd5b792889f515c31ceaad
hendrysiak <lukasz.hendrysiak@gmail.com>
2019-12-26 11:50:03 +0100

    Created endpoint's description and fixed repository controllers

--- server/src/routes/repository.js
+++ server/src/routes/repository.js
@@ -2,25 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Controller = require('../controllers/repository');
 
-const multer = require("multer");
-
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, './icons');
-    },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname);
-    }
-})
-
-const upload = multer({
-    storage
-});
-
 
 
 router.route('/')
-    .post(upload.single("repository"), Controller.createNew)
+    .post(Controller.createNew)
     .get(Controller.getAll)
 
 router.route('/:repository_id')
