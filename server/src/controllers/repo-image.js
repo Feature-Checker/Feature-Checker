a368484edf44713634ca3b41868a6be5e8c58ce0
hendrysiak <lukasz.hendrysiak@gmail.com>
2020-01-08 14:43:56 +0100

    Fixed image downloader

--- server/src/controllers/repo-image.js
+++ server/src/controllers/repo-image.js
@@ -1,17 +1,19 @@
 const imageSearch = require("image-search-google");
-const fs = require("fs");
-const path = require("path");
-const axios = require("axios");
 
-const fetchImage = async (req, res) => {
+const fetchImage = async(req, res) => {
     try {
         console.log(req.query)
         console.log(req.params)
         console.log(process.env)
         const searchPhrase = `${req.query.search} logo`
-        const { API_KEY_GOOGLE, CSE_ID_GOOGLE } = process.env;
+        const {
+            API_KEY_GOOGLE,
+            CSE_ID_GOOGLE
+        } = process.env;
         const client = new imageSearch(CSE_ID_GOOGLE, API_KEY_GOOGLE);
-        const options = { page: 1 };
+        const options = {
+            page: 1
+        };
         const searchResult = await client.search(searchPhrase, options)
         searchResult.map((result, index) => {
             result.id = index + 1
@@ -22,27 +24,7 @@ const fetchImage = async (req, res) => {
     }
 }
 
-const saveImage = async(req, res) => {
-    const url = req.query.url;
-    const name = req.query.name;
-    const rootPath = path.dirname(require.main.filename || process.mainModule.filename);
-    !fs.existsSync(`${rootPath}/logos`) && fs.mkdirSync(`${rootPath}/logos`);
-    const filePath = path.resolve(rootPath, 'logos', `${name}`);
-    const writer = fs.createWriteStream(filePath);
-    try {
-        const icon = await axios({
-            url,
-            method: 'GET',
-            responseType: 'stream'
-        })
-        await icon.data.pipe(writer);
-        await res.send(icon.data)
-    } catch (err) {
-        await res.sendStatus(404);
-    }
-};
 
 module.exports = {
     fetchImage,
-    saveImage
 }
--- server/src/controllers/repository.js
+++ server/src/controllers/repository.js
@@ -1,20 +1,35 @@
+const fs = require("fs");
+const path = require("path");
+const axios = require("axios");
+
 const Repository = require('../models/repository')
 
-const createNew = async (req, res) => {
+const createNew = async(req, res) => {
+    const name = req.body.name;
+    const url = req.body.url;
+    const rootPath = path.dirname(require.main.filename || process.mainModule.filename) + '/logos';
+    !fs.existsSync(`${rootPath}`) && fs.mkdirSync(`${rootPath}`);
     try {
+        const logo = await axios({
+            url,
+            method: 'GET',
+            responseType: 'stream'
+        })
+        await logo.data.pipe(writer);
+        await res.send(logo.data)
         const repository = await new Repository({
-            name: req.body.name,
-            icon: req.body.icon
+            name: name,
+            icon: `${rootPath}/${name}`
         })
         const result = await repository.save()
-        // await res.send(req.file);
+            // await res.send(req.file);
         res.json(result)
     } catch (err) {
         res.send(err)
     }
 }
 
-const getAll = async (req, res) => {
+const getAll = async(req, res) => {
     try {
         const repositories = await Repository.find()
         res.json(repositories)
@@ -23,7 +38,7 @@ const getAll = async (req, res) => {
     }
 }
 
-const getOne = async (req, res) => {
+const getOne = async(req, res) => {
     try {
         const repository = await Repository.findById(req.params.test_id)
         res.json(repository)
@@ -32,7 +47,7 @@ const getOne = async (req, res) => {
     }
 }
 
-const updateOne = async (req, res) => {
+const updateOne = async(req, res) => {
     try {
         const repository = await Repository.findByIdAndUpdate(req.params.test_id, {
             name: req.body.name
@@ -46,7 +61,7 @@ const updateOne = async (req, res) => {
     }
 }
 
-const deleteOne = async (req, res) => {
+const deleteOne = async(req, res) => {
     try {
         await Repository.remove({
             _id: req.params.test_id
