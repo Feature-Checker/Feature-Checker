e9ba9f63376688f4032191683aa7cc01a17550b8
hendrysiak <lukasz.hendrysiak@gmail.com>
2020-01-09 11:53:09 +0100

    Used destructuring, shortcuts

--- server/src/controllers/repository.js
+++ server/src/controllers/repository.js
@@ -1,78 +1,73 @@
-const fs = require("fs");
 const path = require("path");
 const axios = require("axios");
 
-const Repository = require('../models/repository')
+const Repository = require("../models/repository");
 
 const createNew = async(req, res) => {
-    const name = req.body.name;
-    const url = req.body.url;
-    const rootPath = path.dirname(require.main.filename || process.mainModule.filename) + '/logos';
-    !fs.existsSync(`${rootPath}`) && fs.mkdirSync(`${rootPath}`);
+    const { name, url } = req.body;
+    const rootPath =
+        path.dirname(require.main.filename || process.mainModule.filename) +
+        "/logos";
     try {
-        const logo = await axios({
-            url,
-            method: 'GET',
-            responseType: 'stream'
-        })
+        const logo = await axios.get(url, {
+            responseType: "stream"
+        });
         await logo.data.pipe(writer);
-        await res.send(logo.data)
         const repository = await new Repository({
-            name: name,
+            name,
             icon: `${rootPath}/${name}`
-        })
-        const result = await repository.save()
-            // await res.send(req.file);
-        res.json(result)
+        });
+        const result = await repository.save();
+        res.json(result);
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
-}
+};
 
 const getAll = async(req, res) => {
     try {
-        const repositories = await Repository.find()
-        res.json(repositories)
+        const repositories = await Repository.find();
+        res.json(repositories);
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
-}
+};
 
 const getOne = async(req, res) => {
     try {
-        const repository = await Repository.findById(req.params.test_id)
-        res.json(repository)
+        const repository = await Repository.findById(req.params.test_id);
+        res.json(repository);
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
-}
+};
 
 const updateOne = async(req, res) => {
     try {
         const repository = await Repository.findByIdAndUpdate(req.params.test_id, {
             name: req.body.name
-        })
-        await repository.save()
+        });
+        await repository.save();
         res.json({
-            message: 'repository updated'
-        })
+            message: "repository updated"
+        });
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
-}
+};
 
 const deleteOne = async(req, res) => {
     try {
         await Repository.remove({
             _id: req.params.test_id
-        })
+        });
         res.json({
-            message: 'Successfully deleted'
-        })
+            message: "Successfully deleted"
+        });
     } catch (err) {
-        res.send(err)
+        res.send(err);
     }
-}
+};
 
 module.exports = {
     createNew,
@@ -80,4 +75,4 @@ module.exports = {
     getOne,
     updateOne,
     deleteOne
-}
+};
